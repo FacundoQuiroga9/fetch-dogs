782575bac05ebcce0d9719c0188ebed5f4fc4c7a
Facundo Quiroga <facu.quirogaga@gmail.com>
2025-02-07 15:30:04 -0600

    updated login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import "./Login.css"
 
 const Login = () => {
   const { login } = useContext(AuthContext);
@@ -15,12 +16,23 @@ const Login = () => {
 
   return (
     <div className="container mt-5">
-      <h2>Log in</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
-        <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <button type="submit">Sign in</button>
-      </form>
+      <div className="row w-50 login-container">
+        <h2>Log in</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col">
+              <input className="form-control" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
+            </div>
+            <div className="col">
+              <input className="form-control" type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </div>
+          </div>
+          <div className="col d-flex justify-content-center">
+            <button className="fetch-btn" type="submit">Sign in</button>
+          </div>
+        </form>
+      </div>
+
     </div>
   );
 };
